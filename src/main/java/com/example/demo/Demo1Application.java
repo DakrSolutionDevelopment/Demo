b5aab040ef95d500ef68ab0cae369b7da5a765c5
DakrSolutionDevelopment <154223043+DakrSolutionDevelopment@users.noreply.github.com>
2023-12-18 16:07:42 +0530

    Update Demo1Application.java
    comments

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -9,7 +9,7 @@ public class Demo1Application {
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
         System.out.println("Hello");
-        System.out.println("demo");
+        //System.out.println("demo");
 
     }
 
